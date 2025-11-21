8a2282a135bde94b9be185336cbea7a0b5971821
KevinCaballero00 <kevinarturocc@ufps.edu.co>
2025-11-21 16:34:25 -0500

    Railway deployment 5

--- data/php/config.php
+++ data/php/config.php
@@ -1,15 +1,17 @@
 <?php
 
-$host = "mysql.railway.internal";
-$user = "root";
-$password = "UZwrrEhfbXRZmtwbGyluwOhJGxXgFVvV";
-$database = "railway";
-$port = getenv("3306");
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$database = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
+
+// Debug temporal (puedes borrarlo luego)
+// var_dump($host, $user, $password, $database, $port);
 
 $conn = new mysqli($host, $user, $password, $database, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
